ab84cf193f9ad5d8ef0eea783538f7bdb5495c3a
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-23 12:32:09 -0500

    Search Bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Search, Grid, Header, Icon } from 'semantic-ui-react'
+import './SearchBar.scss'
 
-const SearchBar = () => {
-    return (
-        <div>
-            <h1>HOlaaaaaaa</h1>
-        </div>
-    )
-}
+const SearchBar = () => (
+  <Header block>
+    <Grid>
+      <Grid.Column width={2} textAlign="center">
+        <Icon name="basketball ball" size="big" color="orange" />
+        <Header.Content>NBA PAIRS</Header.Content>
+      </Grid.Column>
+      <Grid.Column textAlign="center" width="10">
+        <Search />
+      </Grid.Column>
+      <Grid.Column width={4} floated="left">
+        <img
+          className="searchbar-match-icon"
+          src="//images.squarespace-cdn.com/content/v1/5f7c74b3ebc1ba132269c5a7/1611678115046-8C180XU5WIJ07PA60ADA/mach_8_logo_highlight_full.jpg?format=1500w"
+          alt="Mach Eight, LLC"
+        />
+      </Grid.Column>
+    </Grid>
+  </Header>
+)
 
 export default SearchBar
--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { shallow } from 'enzyme';
-import SearchBar from './SearchBar';
-
+import { shallow } from 'enzyme'
+import SearchBar from './SearchBar'
 
 describe('SearchBar', () => {
-    test('should match to snapshoot ', () => {
-    const wrapper = shallow(<SearchBar/>)
-    expect(wrapper).toMatchSnapshot()        
-    });
-});
+  test('should match to snapshoot', () => {
+    const wrapper = shallow(<SearchBar />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
