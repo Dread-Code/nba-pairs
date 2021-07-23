4a316a0ae1570acb4bf98f0562a11ce7ab2f75a4
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-23 13:02:07 -0500

    SearchBar added

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,20 +4,22 @@ import './SearchBar.scss'
 
 const SearchBar = () => (
   <Header block>
-    <Grid>
-      <Grid.Column width={2} textAlign="center">
+    <Grid doubling>
+      <Grid.Column width={4} verticalAlign="middle">
         <Icon name="basketball ball" size="big" color="orange" />
         <Header.Content>NBA PAIRS</Header.Content>
       </Grid.Column>
-      <Grid.Column textAlign="center" width="10">
-        <Search />
+      <Grid.Column textAlign="center" width={8}>
+        <Search size="large" />
       </Grid.Column>
       <Grid.Column width={4} floated="left">
-        <img
-          className="searchbar-match-icon"
-          src="//images.squarespace-cdn.com/content/v1/5f7c74b3ebc1ba132269c5a7/1611678115046-8C180XU5WIJ07PA60ADA/mach_8_logo_highlight_full.jpg?format=1500w"
-          alt="Mach Eight, LLC"
-        />
+        <a href="https://www.macheight.com/">
+          <img
+            className="searchbar-match-icon"
+            src="//images.squarespace-cdn.com/content/v1/5f7c74b3ebc1ba132269c5a7/1611678115046-8C180XU5WIJ07PA60ADA/mach_8_logo_highlight_full.jpg?format=1500w"
+            alt="Mach Eight, LLC"
+          />
+        </a>
       </Grid.Column>
     </Grid>
   </Header>
